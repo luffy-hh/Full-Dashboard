0bce77e057a92cd71aab8276b615fa8e6ff1d2a6
Ye Yint Ko <yeyintko.mkn@gmail.com>
2024-01-07 19:05:05 +0630

    fixed grpc

--- slots/slotegrator/callbackService.js
+++ slots/slotegrator/callbackService.js
@@ -1,71 +1,82 @@
 const TransactionRecord = require("../../transaction-record/transactionRecordModel");
 const User = require("../../users/userModels");
+const helper = require("../../utils/helper");
+exports.getUserBalance = async (userId) => {
+  const user = await User.findOne({ userId: userId });
+  let user_data = {
+    id: 445,
+    userId: user.userId,
+    role: user.role,
+    unit: helper.decimal(user.unit, 4),
+    // gameUnit: user.gameUnit,
+    // promotionUnit: user.promotionUnit,
+  };
 
-exports.getUserBalance= async(userId)=>{
-    const user = await User.findOne({ userId: userId });
-    let user_data = {
-        id: 445,
-        userId:user.userId,
-        role:user.role,
-        unit:user.unit,
-        gameUnit:user.gameUnit,
-        promotionUnit:user.promotionUnit
-    }
-  
-    return user_data;
-}
-
+  return user_data;
+};
 
 //when player trying to make a bet.
-exports.betSlot = async(userId,bet_amount)=>{
-    //reduce user's balance, send ag commission ,etc...
-    let user = await User.findOneAndUpdate({userId:userId},{$inc:{unit:-bet_amount}},{new:true});
-    let transaction = await TransactionRecord.create({
-        user_id:user._id,
-        before_amt:user.unit+bet_amount,
-        action_amt:bet_amount,
-        after_amt:user.unit,
-        type:'slot-bet',
-        status:'Out'
-    });
-    return {
-        balance:user.unit,
-        transaction_id:transaction.id
-    }
-}
+exports.betSlot = async (userId, bet_amount) => {
+  //reduce user's balance, send ag commission ,etc...
+  let user = await User.findOneAndUpdate(
+    { userId: userId },
+    { $inc: { unit: -bet_amount } },
+    { new: true }
+  );
+  let transaction = await TransactionRecord.create({
+    user_id: user._id,
+    before_amt: user.unit + bet_amount,
+    action_amt: bet_amount,
+    after_amt: user.unit,
+    type: "slot-bet",
+    status: "Out",
+  });
+  return {
+    balance: helper.decimal(user.unit, 4),
+    transaction: helper.convertTransactionMsg(transaction),
+  };
+};
 
 //when player win in a game
-exports.winSlot = async(userId,win_amount)=>{
-    //increase user's balance, record transaction,etc...
-    let user = await User.findOneAndUpdate({userId:userId},{$inc:{unit:win_amount}},{new:true});
-    let transaction = await TransactionRecord.create({
-        user_id:user._id,
-        before_amt:user.unit-win_amount,
-        action_amt:win_amount,
-        after_amt:user.unit,
-        type:'slot-win',
-        status:'In'
-    });
-    return {
-        balance:user.unit,
-        transaction_id:transaction.id
-    }
-}
+exports.winSlot = async (userId, win_amount) => {
+  //increase user's balance, record transaction,etc...
+  let user = await User.findOneAndUpdate(
+    { userId: userId },
+    { $inc: { unit: win_amount } },
+    { new: true }
+  );
+  let transaction = await TransactionRecord.create({
+    user_id: user._id,
+    before_amt: user.unit - win_amount,
+    action_amt: win_amount,
+    after_amt: user.unit,
+    type: "slot-win",
+    status: "In",
+  });
+  return {
+    balance: helper.decimal(user.unit, 4),
+    transaction: helper.convertTransactionMsg(transaction),
+  };
+};
 
 //Refund is a cash back in case bet problems.
-exports.refundSlot = async(userId,refund_amount)=>{
-    //increase user's balance, record transaction,etc...
-    let user = await User.findOneAndUpdate({userId:userId},{$inc:{unit:refund_amount}},{new:true});
-    let transaction = await TransactionRecord.create({
-        user_id:user._id,
-        before_amt:user.unit-refund_amount,
-        action_amt:refund_amount,
-        after_amt:user.unit,
-        type:'slot-refund',
-        status:'In'
-    });
-    return {
-        balance:user.unit,
-        transaction_id:transaction.id
-    }
-}
+exports.refundSlot = async (userId, refund_amount) => {
+  //increase user's balance, record transaction,etc...
+  let user = await User.findOneAndUpdate(
+    { userId: userId },
+    { $inc: { unit: refund_amount } },
+    { new: true }
+  );
+  let transaction = await TransactionRecord.create({
+    user_id: user._id,
+    before_amt: user.unit - refund_amount,
+    action_amt: refund_amount,
+    after_amt: user.unit,
+    type: "slot-refund",
+    status: "In",
+  });
+  return {
+    balance: helper.decimal(user.unit, 4),
+    transaction: helper.convertTransactionMsg(transaction),
+  };
+};
