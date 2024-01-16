7699da527ca4a71eb261f1618246ce3b6288316a
hhluffy <heinhtetsoe2566@gmail.com>
2024-01-16 11:38:04 +0630

    https and options removed and socket added

--- server.js
+++ server.js
@@ -25,38 +25,38 @@ mongoose
 const port = process.env.PORT || 3000;
 const http_server = http.createServer(app);
 
-// const io = new Server(http_server);
+const io = new Server(http_server);
 
-// let tableRooms = {}; // Object to store players in each table
+let tableRooms = {}; // Object to store players in each table
 
-// io.on("connection", (socket) => {
-//   console.log("A user connected");
+io.on("connection", (socket) => {
+  console.log("A user connected");
 
-//   // Handle table joining
-//   socket.on("joinTable", (tableId) => {
-//     socket.join(tableId);
-//     console.log(`User joined table: ${tableId}`);
+  // Handle table joining
+  socket.on("joinTable", (tableId) => {
+    socket.join(tableId);
+    console.log(`User joined table: ${tableId}`);
 
-//     // Initialize or update the table room data
-//     if (!tableRooms[tableId]) {
-//       tableRooms[tableId] = { players: [] };
-//     }
+    // Initialize or update the table room data
+    if (!tableRooms[tableId]) {
+      tableRooms[tableId] = { players: [] };
+    }
 
-//     // Add player to the room
-//     tableRooms[tableId].players.push(socket.id);
-//   });
+    // Add player to the room
+    tableRooms[tableId].players.push(socket.id);
+  });
 
-//   // Disconnect event
-//   socket.on("disconnect", () => {
-//     console.log("User disconnected");
+  // Disconnect event
+  socket.on("disconnect", () => {
+    console.log("User disconnected");
 
-//     // Remove the disconnected player from the table room
-//     for (const tableId in tableRooms) {
-//       tableRooms[tableId].players = tableRooms[tableId].players.filter(
-//         (playerId) => playerId !== socket.id
-//       );
-//     }
-//   });
-// });
+    // Remove the disconnected player from the table room
+    for (const tableId in tableRooms) {
+      tableRooms[tableId].players = tableRooms[tableId].players.filter(
+        (playerId) => playerId !== socket.id
+      );
+    }
+  });
+});
 
 http_server.listen(port, () => console.log("Listen Now", port));
