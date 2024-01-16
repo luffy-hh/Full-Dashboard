45ad01717eaac32f7e89e526ca75d6e4d360ab03
hhluffy <heinhtetsoe2566@gmail.com>
2024-01-16 11:32:49 +0630

    socket closed

--- server.js
+++ server.js
@@ -8,17 +8,12 @@ const app = require("./app");
 const fs = require("fs");
 require("./slots/grpc-services/grpc");
 dotenv.config({ path: "./config.env" });
-let options = {};
-process.env.NODE_ENV === "development"
-  ? (options = {})
-  : (options = {
-      key: fs.readFileSync(
-        "/etc/letsencrypt/live/gamevegas.online/privkey.pem"
-      ),
-      cert: fs.readFileSync(
-        "/etc/letsencrypt/live/gamevegas.online/fullchain.pem"
-      ),
-    });
+// let options = {};
+
+// options = {
+//   key: fs.readFileSync("/etc/letsencrypt/live/gamevegas.online/privkey.pem"),
+//   cert: fs.readFileSync("/etc/letsencrypt/live/gamevegas.online/fullchain.pem"),
+// };
 
 mongoose
   .connect(process.env.DATABASE_LOCAL, {
@@ -28,42 +23,40 @@ mongoose
   .then(() => console.log("DB Connection Success"));
 
 const port = process.env.PORT || 3000;
-const http_server =
-  process.env.NODE_ENV === "development"
-    ? http.createServer(options, app)
-    : https.createServer(options, app);
-const io = new Server(http_server);
+const http_server = http.createServer(options, app);
 
-let tableRooms = {}; // Object to store players in each table
+// const io = new Server(http_server);
 
-io.on("connection", (socket) => {
-  console.log("A user connected");
+// let tableRooms = {}; // Object to store players in each table
 
-  // Handle table joining
-  socket.on("joinTable", (tableId) => {
-    socket.join(tableId);
-    console.log(`User joined table: ${tableId}`);
+// io.on("connection", (socket) => {
+//   console.log("A user connected");
 
-    // Initialize or update the table room data
-    if (!tableRooms[tableId]) {
-      tableRooms[tableId] = { players: [] };
-    }
+//   // Handle table joining
+//   socket.on("joinTable", (tableId) => {
+//     socket.join(tableId);
+//     console.log(`User joined table: ${tableId}`);
 
-    // Add player to the room
-    tableRooms[tableId].players.push(socket.id);
-  });
+//     // Initialize or update the table room data
+//     if (!tableRooms[tableId]) {
+//       tableRooms[tableId] = { players: [] };
+//     }
 
-  // Disconnect event
-  socket.on("disconnect", () => {
-    console.log("User disconnected");
+//     // Add player to the room
+//     tableRooms[tableId].players.push(socket.id);
+//   });
 
-    // Remove the disconnected player from the table room
-    for (const tableId in tableRooms) {
-      tableRooms[tableId].players = tableRooms[tableId].players.filter(
-        (playerId) => playerId !== socket.id
-      );
-    }
-  });
-});
+//   // Disconnect event
+//   socket.on("disconnect", () => {
+//     console.log("User disconnected");
+
+//     // Remove the disconnected player from the table room
+//     for (const tableId in tableRooms) {
+//       tableRooms[tableId].players = tableRooms[tableId].players.filter(
+//         (playerId) => playerId !== socket.id
+//       );
+//     }
+//   });
+// });
 
 http_server.listen(port, () => console.log("Listen Now", port));
