8d75cbc897279acb6529f3823a66c9a8c4e550e5
hhluffy <heinhtetsoe2566@gmail.com>
2024-01-22 00:40:16 +0630

    cors added

--- app.js
+++ app.js
@@ -102,7 +102,19 @@ app.use((req, res, next) => {
 //});
 
 //app.use("/api", limiter);
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.header('Access-Control-Allow-Credentials', true);
 
+  // Handle preflight requests
+  if (req.method === 'OPTIONS') {
+    res.sendStatus(200);
+  } else {
+    next();
+  }
+});
 app.use(express.json());
 
 // app.use(mongoSanitize());
