0a22d5631747023bac29c966e67831ae85803390
hhluffy <heinhtetsoe2566@gmail.com>
2024-01-28 18:47:07 +0630

    fix some more

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
 // "undefined" means the URL will be computed from the `window.location` object
-const URL = "https://gamevegas.online/dashboard";
+const URL = "https://ar7mm.org/dashboard";
 
 export const socket = io(URL);
