d128895c46660840df7cff11d8dfa005cc212e38
hhluffy <heinhtetsoe2566@gmail.com>
2024-01-31 15:02:05 +0630

    feature: transaction record for user added

--- transaction-record/transactionRecordRoute.js
+++ transaction-record/transactionRecordRoute.js
@@ -1,8 +1,15 @@
-const express = require('express');
+const express = require("express");
 const userController = require("../users/userControllers");
-const {getAllTransactionRecord} = require("./transactionRecordController");
+const { getAllTransactionRecord } = require("./transactionRecordController");
 const router = express.Router();
 
-router.route('/').get(userController.protect,userController.restrictTo("Admin","Master","Agent"),getAllTransactionRecord)
+router
+  .route("/")
+  .get(
+    userController.protect,
+    userController.restrictTo("Admin", "Master", "Agent"),
+    getAllTransactionRecord
+  );
+router.route("/my-own").get(userController.protect, getRelatedRecordForUser);
 
 module.exports = router;
