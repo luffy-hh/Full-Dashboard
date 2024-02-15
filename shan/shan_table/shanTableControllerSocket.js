58be78c7579f025089a17fb1a9d4b77a5d6ad4c8
hhluffy <heinhtetsoe2566@gmail.com>
2024-02-15 22:03:50 +0630

    tab update

--- shan/shan_table/shanTableControllerSocket.js
+++ shan/shan_table/shanTableControllerSocket.js
@@ -1,108 +1,45 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+const tableGetter = require("./tableGetter");
 const ShanTable = require("./shanTableModel");
-const AppError = require("../../utils/appError");
-const catchAsync = require("../../utils/catchAsync");
 
-// Create Shan Table
-exports.createShanTable = catchAsync(async (req, res) => {
+exports.createTableData = async (socket, data) => {
   try {
-    const { tableName, role, description } = req.body;
-
-    const insertObj = {
-      tableName,
-      role,
-      description,
-    };
-
-    const newTable = await ShanTable.create({ ...insertObj });
-    newTable.endPoint = `/${newTable._id.toString()}`;
+    const newTable = new ShanTable({
+      tableName: data.tableName,
+      role: data.role,
+      description: data.description,
+    });
     await newTable.save();
-    res.status(201).json({
-      status: "success",
-      data: {
-        newTable,
-      },
-    });
-  } catch (err) {
-    res.status(404).json({
-      status: "fail",
-      message: err.message,
-    });
-  }
-});
 
-exports.readShanRole = catchAsync(async (req, res) => {
+    return newTable;
+  } catch (error) {
+    console.error("Error parsing JSON data:", error);
+  }
+};
+
+exports.readTableData = async (socket, data) => {
   try {
-    const allShanRole = await ShanRole.find({});
-
-    res.status(201).json({
-      status: "success",
-      length: allShanRole.length,
-      data: {
-        allShanRole,
-      },
-    });
-  } catch (err) {
-    res.status(404).json({
-      status: "fail",
-      message: err,
-    });
+    if (data.idValue === "all") {
+      const tablesValue = await tableGetter.responseTableAll();
+      socket.emit("responseTableDataAll", {
+        length: tablesValue.length,
+        tableDataAll: tablesValue,
+      });
+    } else if (data.idValue !== "all" && data.idValue) {
+      const tablesValue = await tableGetter.responseTableByRole(data.idValue);
+      socket.emit("responseTableDataAll", {
+        length: tablesValue.length,
+        tableDataAll: tablesValue,
+      });
+    }
+  } catch (error) {
+    console.error("Error parsing JSON data:", error);
   }
-});
-
-// Update Shan Roll
-exports.updateShanTable = catchAsync(async (req, res) => {
-  try {
-    // Extract user ID from token
-    const token = req.headers.authorization.split(" ")[1];
-    const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
-    const currentUserId = decoded.id;
-
-    // Find the ShanRole document by ID
-    const shanRoleId = req.params.id;
-    const shanRole = await ShanRole.findById(shanRoleId);
-
-    // Check if the ShanRole document exists
-    if (!shanRole) {
-      throw new AppError("ShanRole not found", 404);
-    }
-
-    // Update ShanRole properties
-    const reqBody = { ...req.body, ownderData: currentUserId };
-
-    // Check if an image file is provided in the update
-    if (req.file) {
-      reqBody.img = req.file.filename;
-    }
-
-    // Update the ShanRole document
-    const updatedShanRole = await ShanRole.findByIdAndUpdate(
-      shanRoleId,
-      reqBody,
-      {
-        new: true,
-        runValidators: true,
-      }
-    );
-
-    // Build the image URL link if an image was provided in the update
-    let imageLink;
-    if (req.file) {
-      imageLink = `${req.protocol}://${req.get("host")}/images/shan_roll/${
-        req.file.filename
-      }`;
-    }
-
-    res.status(200).json({
-      status: "success",
-      data: {
-        updatedShanRole,
-        imageLink,
-      },
-    });
-  } catch (err) {
-    res.status(err.statusCode || 500).json({
-      status: "fail",
-      message: err.message || "Internal Server Error",
-    });
-  }
-});
+};
