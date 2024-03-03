5e356fcdd18bc999fbd1fedf330973101ff8010b
hhluffy <heinhtetsoe2566@gmail.com>
2024-03-03 22:13:41 +0630

    game soft update 12

--- game_soft/game_result/gameResultController.js
+++ game_soft/game_result/gameResultController.js
@@ -3,21 +3,39 @@ const GameSoftProvider = require("../providers/providersModels");
 
 exports.gameResult = async (req, res) => {
   try {
-    console.log(req.body);
-    // const validUser = await User.findOne({ userId: req.body.MemberName });
-    // const providerObj = await GameSoftProvider.findOne({
-    //   providerCode: req.body.ProductID,
-    // });
-    // const convertUnit = validUser.unit / providerObj.conversionRate;
-    // if (validUser) {
-    //   const resData = {
-    //     ErrorCode: 0,
-    //     ErrorMessage: "Success",
-    //     Balance: convertUnit,
-    //     BeforeBalance: 0,
-    //   };
-    //   res.json(resData);
-    // }
+    const data = req.body;
+    const providerObj = await GameSoftProvider.findOne({
+      providerCode: data.ProductID,
+    });
+    const currentUserObj = User.findOne({ userId: data.MemberName });
+    const convertUnit = currentUserObj.unit / providerObj.conversionRate;
+    const winLoseResult =
+      data.Transactions[0].PayoutAmount == 0 ? "lose" : "win";
+    const resData = {
+      ErrorCode: 16,
+      ErrorMessage: "Failed",
+      Balance: convertUnit,
+      BeforeBalance: convertUnit + data.Transactions[0].BetAmount,
+    };
+    if (winLoseResult === "lose") {
+      resData.ErrorCode = 16;
+      resData.ErrorMessage = "Failed";
+      resData.Balance = convertUnit;
+      resData.BeforeBalance = convertUnit + data.Transactions[0].BetAmount;
+    } else {
+      resData.ErrorCode = 0;
+      resData.ErrorMessage = "Success";
+      resData.Balance =
+        convertUnit +
+        data.Transactions[0].BetAmount +
+        data.Transactions[0].PayoutAmount;
+      resData.BeforeBalance = convertUnit + data.Transactions[0].BetAmount;
+
+      await User.findByIdAndUpdate(User._id, {
+        unit: resData.Balance * providerObj.conversionRate,
+      });
+    }
+    res.json(resData);
   } catch (error) {
     console.error("Error:", error.message);
     res.status(500).json({ error: "Internal Server Error" });
