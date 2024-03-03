ccbe396ca90a37234f2a13069df08140ba1bdd47
hhluffy <heinhtetsoe2566@gmail.com>
2024-03-03 15:36:08 +0630

    game soft update 6

--- game_soft/place_bet/placeBetRoutes.js
+++ game_soft/place_bet/placeBetRoutes.js
@@ -3,8 +3,6 @@ const placeBetControllers = require("./placeBetControllers");
 const userController = require("../../users/userControllers");
 
 const router = express.Router();
-router
-  .route("/Seamless/PlaceBet")
-  .post(userController.protect, placeBetControllers.placeBet);
+router.route("/Seamless/PlaceBet").post(placeBetControllers.placeBet);
 
 module.exports = router;
